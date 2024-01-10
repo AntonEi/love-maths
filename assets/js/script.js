4c898466a57ecd28c0118a53ab0b0fc07de02df4
AntonEi <>
2024-01-10 09:45:22 +0000

    Set up stucture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function caculateCorrectAnswer() {
+
+}
+
+function incremnetScore() {
+
+}
+
+function incremnetWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
